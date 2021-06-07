4593974ea47f013f7a11fb8a0eeaf28a00c8b41d
Anderson Alves Nunes de Freitas <anunes@instructure.com>
2021-06-06 23:07:42 -0300

    pushing build for github pages test

---
+++ github-pages/assets/src_translations_pt-BR_json.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkfrontend_login"] = self["webpackChunkfrontend_login"] || []).push([["src_translations_pt-BR_json"],{
+
+/***/ "./src/translations/pt-BR.json":
+/*!*************************************!*\
+  !*** ./src/translations/pt-BR.json ***!
+  \*************************************/
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = JSON.parse('{\"Your email\":{\"message\":\"Seu email\"}}');\n\n//# sourceURL=webpack://frontend-login/./src/translations/pt-BR.json?");
+
+/***/ })
+
+}]);
