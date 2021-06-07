3f4e63b6f63a479333b2035b7a0f3bb85da2b6c2
Anderson Alves Nunes de Freitas <anunes@instructure.com>
2021-06-07 03:17:38 -0300

    readd assets for gh pages

---
+++ github-pages/assets/src_Login_tsx.js
@@ -0,0 +1,76 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkfrontend_login"] = self["webpackChunkfrontend_login"] || []).push([["src_Login_tsx"],{
+
+/***/ "./src/Login.tsx":
+/*!***********************!*\
+  !*** ./src/Login.tsx ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"webpack/sharing/consume/default/react/react?270f\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var mdbreact__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! mdbreact */ \"webpack/sharing/consume/default/mdbreact/mdbreact\");\n/* harmony import */ var mdbreact__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(mdbreact__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _initializers_runInitializers__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./initializers/runInitializers */ \"./src/initializers/runInitializers.ts\");\n/* harmony import */ var _fortawesome_fontawesome_free_css_all_min_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @fortawesome/fontawesome-free/css/all.min.css */ \"./node_modules/@fortawesome/fontawesome-free/css/all.min.css\");\n/* harmony import */ var bootstrap_css_only_css_bootstrap_min_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! bootstrap-css-only/css/bootstrap.min.css */ \"./node_modules/bootstrap-css-only/css/bootstrap.min.css\");\n/* harmony import */ var mdbreact_dist_css_mdb_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! mdbreact/dist/css/mdb.css */ \"./node_modules/mdbreact/dist/css/mdb.css\");\n/* harmony import */ var _Login_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./Login.css */ \"./src/Login.css\");\n\n\n\n\n\n\n\n\nconst Login = () => {\n  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {\n    (0,_initializers_runInitializers__WEBPACK_IMPORTED_MODULE_2__.default)({\n      locale: 'pt-BR'\n    });\n  }, []);\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBContainer, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBRow, {\n    center: true,\n    className: \"mt-5\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBCol, {\n    md: \"5\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBCard, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"header pt-3 grey lighten-2\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBRow, {\n    className: \"d-flex justify-content-start\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h3\", {\n    className: \"deep-grey-text mt-3 mb-4 pb-1 mx-5\"\n  }, \"Log in\"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBCardBody, {\n    className: \"mx-4 mt-4\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBInput, {\n    label: \"Your email\",\n    group: true,\n    type: \"text\",\n    validate: true\n  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBInput, {\n    label: \"Your password\",\n    group: true,\n    type: \"password\",\n    validate: true,\n    containerClass: \"mb-0\"\n  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", {\n    className: \"font-small grey-text d-flex justify-content-end\"\n  }, \"Forgot\", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"a\", {\n    href: \"#!\",\n    className: \"dark-grey-text font-weight-bold ml-1\"\n  }, \"Password?\")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"text-center mb-4 mt-5\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(mdbreact__WEBPACK_IMPORTED_MODULE_1__.MDBBtn, {\n    color: \"danger\",\n    type: \"button\",\n    className: \"btn-block z-depth-2\"\n  }, \"Log in\")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", {\n    className: \"font-small grey-text d-flex justify-content-center\"\n  }, \"Don't have an account?\", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"a\", {\n    href: \"#!\",\n    className: \"dark-grey-text font-weight-bold ml-1\"\n  }, \"Sign up\")))))));\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Login);\n\n//# sourceURL=webpack://frontend-login/./src/Login.tsx?");
+
+/***/ }),
+
+/***/ "./src/initializers/runInitializers.ts":
+/*!*********************************************!*\
+  !*** ./src/initializers/runInitializers.ts ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _setLocale__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./setLocale */ \"./src/initializers/setLocale.ts\");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (({\n  locale = _setLocale__WEBPACK_IMPORTED_MODULE_0__.DEFAULT_LOCALE\n} = {}) => {\n  (0,_setLocale__WEBPACK_IMPORTED_MODULE_0__.default)(locale);\n});\n\n//# sourceURL=webpack://frontend-login/./src/initializers/runInitializers.ts?");
+
+/***/ }),
+
+/***/ "./src/initializers/setLocale.ts":
+/*!***************************************!*\
+  !*** ./src/initializers/setLocale.ts ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"DEFAULT_LOCALE\": () => (/* binding */ DEFAULT_LOCALE),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var format_message__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! format-message */ \"webpack/sharing/consume/default/format-message/format-message\");\n/* harmony import */ var format_message__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(format_message__WEBPACK_IMPORTED_MODULE_0__);\n // import generate from 'format-message-generate-id'\n//\n// t.setup({ generateId: generate.underscored_crc32 })\n\nconst loadedTranslations = {};\n\nconst getNormalizedLocale = locale => {\n  const map = {\n    en: 'en-US'\n  };\n  return map[locale] || locale;\n};\n\nconst loadTranslations = locale => {\n  if (loadedTranslations[locale]) {\n    return Promise.resolve(loadedTranslations[locale]);\n  }\n\n  return __webpack_require__(\"./src/translations lazy recursive ^\\\\.\\\\/.*\\\\.json$\")(`./${locale}.json`).then(module => {\n    loadedTranslations[locale] = module.default;\n    return loadedTranslations[locale];\n  }).catch(() => {\n    console.warn(`[login loadTranslations] Failed to get the ${locale} locale.`);\n    return {};\n  });\n};\n\nconst setLocale = _locale => {\n  const locale = getNormalizedLocale(_locale);\n  return loadTranslations(locale).then(translations => {\n    console.log('translations', translations);\n    format_message__WEBPACK_IMPORTED_MODULE_0___default().setup({\n      locale,\n      translations: {\n        [locale]: translations\n      }\n    });\n  });\n};\n\nconst DEFAULT_LOCALE = 'en';\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (setLocale);\n\n//# sourceURL=webpack://frontend-login/./src/initializers/setLocale.ts?");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/Login.css":
+/*!*************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/Login.css ***!
+  \*************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \".form-simple .font-small {\\n  font-size: 0.8rem; }\\n\\n.form-simple .header {\\n  border-top-left-radius: .3rem;\\n  border-top-right-radius: .3rem; }\\n\\n.form-simple input[type=text]:focus:not([readonly]) {\\n  border-bottom: 1px solid #ff3547;\\n  -webkit-box-shadow: 0 1px 0 0 #ff3547;\\n  box-shadow: 0 1px 0 0 #ff3547; }\\n\\n.form-simple input[type=text]:focus:not([readonly]) + label {\\n  color: #4f4f4f; }\\n\\n.form-simple input[type=password]:focus:not([readonly]) {\\n  border-bottom: 1px solid #ff3547;\\n  -webkit-box-shadow: 0 1px 0 0 #ff3547;\\n  box-shadow: 0 1px 0 0 #ff3547; }\\n\\n.form-simple input[type=password]:focus:not([readonly]) + label {\\n  color: #4f4f4f; }\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://frontend-login/./src/Login.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ }),
+
+/***/ "./src/Login.css":
+/*!***********************!*\
+  !*** ./src/Login.css ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_cjs_js_Login_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./Login.css */ \"./node_modules/css-loader/dist/cjs.js!./src/Login.css\");\n\n            \n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_Login_css__WEBPACK_IMPORTED_MODULE_1__.default, options);\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_Login_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});\n\n//# sourceURL=webpack://frontend-login/./src/Login.css?");
+
+/***/ }),
+
+/***/ "./src/translations lazy recursive ^\\.\\/.*\\.json$":
+/*!****************************************************************!*\
+  !*** ./src/translations/ lazy ^\.\/.*\.json$ namespace object ***!
+  \****************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var map = {\n\t\"./pt-BR.json\": [\n\t\t\"./src/translations/pt-BR.json\",\n\t\t\"src_translations_pt-BR_json\"\n\t]\n};\nfunction webpackAsyncContext(req) {\n\tif(!__webpack_require__.o(map, req)) {\n\t\treturn Promise.resolve().then(() => {\n\t\t\tvar e = new Error(\"Cannot find module '\" + req + \"'\");\n\t\t\te.code = 'MODULE_NOT_FOUND';\n\t\t\tthrow e;\n\t\t});\n\t}\n\n\tvar ids = map[req], id = ids[0];\n\treturn __webpack_require__.e(ids[1]).then(() => {\n\t\treturn __webpack_require__.t(id, 3);\n\t});\n}\nwebpackAsyncContext.keys = () => (Object.keys(map));\nwebpackAsyncContext.id = \"./src/translations lazy recursive ^\\\\.\\\\/.*\\\\.json$\";\nmodule.exports = webpackAsyncContext;\n\n//# sourceURL=webpack://frontend-login/./src/translations/_lazy_^\\.\\/.*\\.json$_namespace_object?");
+
+/***/ })
+
+}]);
